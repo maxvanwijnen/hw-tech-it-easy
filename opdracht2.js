86c767ab2389b82c531564d658f1bdf21886dc69
maxvanwijnen <maxvanwijnen@gmail.com>
2022-06-30 23:22:33 +0200

    opdracht 2c

--- opdracht2.js
+++ opdracht2.js
@@ -1,14 +1,15 @@
 //Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console
 
 //declare variabele waarin we aantal verkochte tvs opslaan
+//voore elke tv in de inventory haal het aantal sold op en  tel het op bij totalTvsSold
+//log het totaalTvsSold
+
 let totalTvsSold = 0;
 
-//voore elke tv in de inventory haal het aantal sold op en  tel het op bij totalTvsSold
 for (let i = 0; i < inventory.length; i++) {
     totalTvsSold = totalTvsSold + inventory[i].sold;
 }
 
-//log het totaalTvsSold
 console.log(totalTvsSold);
 
 
@@ -16,3 +17,19 @@ console.log(totalTvsSold);
 
 const TvsSoldAmountElement = document.getElementById('tvs-sold-amount');
 TvsSoldAmountElement.innerText = totalTvsSold;
+
+
+//Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht?
+//Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
+//declareer een variabele voor het totaal
+//loop alle items langs en tel de originalstock telkens bij het totaal op
+//log het resultaat
+let totalPurchased = 0;
+
+inventory.map((item) => {
+    totalPurchased = totalPurchased + item.originalStock;
+});
+
+console.log(totalPurchased);
+
