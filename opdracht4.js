370efbf072ecce5f4b175fe73b367fd394c0ff0d
maxvanwijnen <maxvanwijnen@gmail.com>
2022-07-02 00:54:50 +0200

    opdracht 4c

--- opdracht4.js
+++ opdracht4.js
@@ -23,3 +23,36 @@ function formatPrice(originalPrice) {
     return `€${originalPrice},-`
 }
 
+//Opdracht 4c: Maak een herbruikbare functie die een string genereert voor alle beschikbare schermgroottes van één tv.
+//De functie geeft dit terug in het format [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches
+// ([schermgrootte omgerekend]cm
+
+//declareer een functie met de parameter type
+//zoek de juiste entry aan de hand van het type
+//declareer een string voor de screensize output
+//loop door de screensize heen
+//declareer een const voor de maat in inches
+//declareer een const voor de maat in cm
+//voeg per gevonden screensize deze netjes toe aan de string
+//voeg een | toe aan het eind van de string, maar doe dat niet bij de laatste
+function generateSizeString(type){
+    const tv = inventory.find((item) => {
+            return item.type === type;
+    });
+
+    let screenSizeString = '';
+    for (let i = 0; i < tv.availableSizes.length ; i++) {
+        const screenSizeInches = tv.availableSizes[i];
+        const screenSizeCM  = screenSizeInches * 2.54;
+
+        screenSizeString = `${screenSizeString} ${screenSizeInches} inch (${screenSizeCM} cm)`;
+
+        if (i < tv.availableSizes.length - 1){
+            screenSizeString = `${screenSizeString} |`
+        }
+
+    }
+
+    return screenSizeString;
+}
+generateSizeString('43PUS6504/12');
